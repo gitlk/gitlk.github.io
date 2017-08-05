d8be258ede3862a7c005867cc9956ca7ff305173
gitlk <913887798@qq.com>
2017-08-05 17:17:54 +0800

    email 配置

--- cform_config.php
+++ cform_config.php
@@ -3,7 +3,7 @@
   
   // configuration
   $page_title = "Contact Us Form";
-  $email_it_to = "YourEmailHere@YourSite.com"; //change to your email here
+  $email_it_to = "913887798@qq.com"; //change to your email here
   $error_message = "Please complete the form first";
   $confirmation = "Thank you, your message has been successfully sent.";
 ?>
